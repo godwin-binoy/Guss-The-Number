ef9f8fc2a9258613e10443e6220a40075767703b
Godwin-Binoy <godwinbinoy72@gmail.com>
2024-04-05 12:42:44 +0530

    Both range have coded

--- Guss The Number.py
+++ Guss The Number.py
@@ -1,1 +1,18 @@
-print('Hello world!')
+print('Guss The Number\n---------------')
+
+import random
+
+while True :
+    print('Enter Lower range of number')
+    lower_range = input('  : ')
+    print('Enter Higher range of number')
+    heigher_range = input('  : ')
+
+    try :
+        lower_range = int(lower_range)
+        heigher_range = int(heigher_range)
+    except :
+        print('Error : Make sure that you entered numbers')
+        continue
+
+    
